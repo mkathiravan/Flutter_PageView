271307006724d1866cb34387e4e4ed02443716f5
Kathiravan <mkathiravanmca@gmail.com>
2021-05-06 14:44:35 +0530

    Initial commit

---
+++ lib/widget/page_circle_indicator.dart
@@ -0,0 +1,28 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:page_view_indicators/circle_page_indicator.dart';
+
+class PageCircleIndicator extends StatelessWidget
+{
+  final ValueNotifier<int> currentPageNotifier;
+  final int itemCount;
+
+  static const double _dotSize = 12.0;
+
+  const PageCircleIndicator({
+    Key key,
+    this.currentPageNotifier,
+    this.itemCount,
+}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => CirclePageIndicator(
+      size: _dotSize,
+      selectedSize: _dotSize,
+      dotColor: Theme.of(context).primaryColor,
+      itemCount: itemCount,
+      currentPageNotifier: currentPageNotifier,
+
+  );
+}
---
+++ lib/widget/page_entry_widget.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_pageindicator/model/page_entry.dart';
+
+class PageEntryWidget extends StatefulWidget {
+  final PageEntry entry;
+  const PageEntryWidget({Key key, this.entry}) : super(key: key);
+
+  @override
+  _PageEntryWidgetState createState() => _PageEntryWidgetState();
+}
+
+class _PageEntryWidgetState extends State<PageEntryWidget> {
+  ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scrollToEnd(milliseconds: 1000);
+  }
+
+  @override
+  Widget build(BuildContext context) => OrientationBuilder(
+      builder: (BuildContext context, Orientation orientation) {
+        scrollToEnd(milliseconds: 100);
+
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            controller: controller,
+            children: <Widget>[
+              Image.asset(widget.entry.image, fit: BoxFit.cover),
+              SizedBox(height: 8.0),
+              Text(widget.entry.title,
+                  style: Theme.of(context).textTheme.title),
+              SizedBox(height: 8.0),
+              Text(widget.entry.description,
+                  style: Theme.of(context).textTheme.body1),
+            ],
+          ),
+        );
+      });
+
+  void scrollToEnd({int milliseconds}) {
+    Timer(
+      Duration(milliseconds: milliseconds),
+          () {
+        if (!controller.hasClients) return;
+        controller.jumpTo(controller.position.maxScrollExtent);
+      },
+    );
+  }
+}
